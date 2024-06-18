02a039c08347b5ba9e8dc0eb5ef67539ac6fa461
Olga Aukhadieva <sarole4ka@gmail.com>
2024-06-18 11:49:33 +0500

    Add Course model

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    img = models.ImageField(upload_to='course/', verbose_name='превью (картинка)', **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
