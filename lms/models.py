d6811b278ef68991fba041581bdd86178f9db3d6
Olga Aukhadieva <sarole4ka@gmail.com>
2024-06-18 12:11:44 +0500

    Add Lesson model

--- lms/models.py
+++ lms/models.py
@@ -15,3 +15,18 @@ class Course(models.Model):
     class Meta:
         verbose_name = 'курс'
         verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    img = models.ImageField(upload_to='course/', verbose_name='превью (картинка)', **NULLABLE)
+    link = models.URLField(max_length=200, verbose_name='ссылка на видео')
+    course = models.ForeignKey(to=Course, on_delete=models.CASCADE, verbose_name='курс')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
