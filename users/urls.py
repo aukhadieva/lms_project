c97154c5e982d628a4eef71dfae8b963925e2b0c
Olga Aukhadieva <sarole4ka@gmail.com>
2024-06-19 20:54:16 +0500

    Add router for PaymentListAPIView

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserUpdateAPIView
-
+from users.views import UserUpdateAPIView, PaymentListAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('user/edit/<int:pk>/', UserUpdateAPIView.as_view(), name='edit_user')
+    path('user/edit/<int:pk>/', UserUpdateAPIView.as_view(), name='edit_user'),
+    path('payments/', PaymentListAPIView.as_view(), name='payments'),
 ]
