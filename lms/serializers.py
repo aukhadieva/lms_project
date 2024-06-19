5eb1e5ab170d18711024b669abd2fc0bddf2e649
Olga Aukhadieva <sarole4ka@gmail.com>
2024-06-19 17:59:27 +0500

    Add get_number_of_lessons (CourseSerializer)

--- lms/serializers.py
+++ lms/serializers.py
@@ -4,10 +4,15 @@ from lms.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    number_of_lessons = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
+    def get_number_of_lessons(self, obj):
+        if obj.lesson_set.all().count():
+            return obj.lesson_set.all().count()
+        return 0
 
 
 class LessonSerializer(serializers.ModelSerializer):
