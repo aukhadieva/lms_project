75a28d8f3f214aa78a8ca181256dbdf84e15f455
Olga Aukhadieva <sarole4ka@gmail.com>
2024-06-19 21:15:12 +0500

    Update UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -3,15 +3,15 @@ from rest_framework import serializers
 from users.models import User, Payment
 
 
-class UserSerializer(serializers.ModelSerializer):
-
+class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = Payment
         fields = '__all__'
 
 
-class PaymentSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    payment_history = PaymentSerializer(source='payment_set', many=True, read_only=True)
 
     class Meta:
-        model = Payment
+        model = User
         fields = '__all__'
