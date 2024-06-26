b3b560b5f069b6ae89bd3870e0c20f6ad0740dd1
Olga Aukhadieva <sarole4ka@gmail.com>
2024-06-26 08:30:22 +0500

    Add field owner to Course and Lesson models

--- lms/models.py
+++ lms/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from config import settings
 
 NULLABLE = {'blank': True, 'null': True}
 
 
 class Course(models.Model):
+    owner = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='владелец', **NULLABLE)
     name = models.CharField(max_length=150, verbose_name='название')
     description = models.TextField(verbose_name='описание')
     img = models.ImageField(upload_to='course/', verbose_name='превью (картинка)', **NULLABLE)
@@ -18,6 +20,7 @@ class Course(models.Model):
 
 
 class Lesson(models.Model):
+    owner = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='владелец', **NULLABLE)
     name = models.CharField(max_length=150, verbose_name='название')
     description = models.TextField(verbose_name='описание')
     img = models.ImageField(upload_to='lesson/', verbose_name='превью (картинка)', **NULLABLE)
