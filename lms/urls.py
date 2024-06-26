0919b02d6400f16fcaceae09036aa2de009db7e7
Olga Aukhadieva <sarole4ka@gmail.com>
2024-06-26 14:24:21 +0500

    Fix PEP8 issues

--- lms/urls.py
+++ lms/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from lms.apps import LmsConfig
 from rest_framework.routers import DefaultRouter
 
-from lms.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, \
-    LessonDestroyAPIView
+from lms.views import (CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView,
+                       LessonUpdateAPIView, LessonDestroyAPIView)
 
 app_name = LmsConfig.name
 
--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 from users.models import User, Payment
-from users.permissions import IsUser
 
 
 class PaymentSerializer(serializers.ModelSerializer):
