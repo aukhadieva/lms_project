72a0234c1f0386ac9171de4fbc7e56f96837af7b
Olga Aukhadieva <sarole4ka@gmail.com>
2024-06-26 09:54:40 +0500

    Add custom permission class IsOwner

--- lms/permissions.py
+++ lms/permissions.py
@@ -5,6 +5,17 @@ class IsModerator(permissions.BasePermission):
 
     def has_permission(self, request, view):
         """
-        Проверяет права принадлежность пользователя к группе 'moderator'.
+        Проверяет принадлежность пользователя к группе 'moderator'.
         """
         return request.user.groups.filter(name='moderator').exists()
+
+
+class IsOwner(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Проверяет, является ли пользователь - владельцем курса/ урока.
+        """
+        if obj.owner == request.user:
+            return True
+        return False
