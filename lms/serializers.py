5117a20c184418d85defe7704ab09032309f13c9
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-01 18:01:56 +0500

    Add validator LinkValidator (LessonSerializer)

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 
 from lms.models import Course, Lesson
+from lms.validators import LinkValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [LinkValidator(field='link')]
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -18,6 +21,9 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_number_of_lessons(self, obj):
+        """
+        Возвращает количество уроков курса.
+        """
         if obj.lesson_set.all().count():
             return obj.lesson_set.all().count()
         return 0
