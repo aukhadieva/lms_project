751018614903c4ef40fe6289fc956592e2e7def9
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-02 11:16:57 +0500

    Add Subscription model

--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from config import settings
+from lms.models import Course
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='пользователь')
+    course = models.ForeignKey(to=Course, on_delete=models.CASCADE, verbose_name='курс')
+    is_subscribed = models.BooleanField(default=False, verbose_name='признак подписки')
+
+    def __str__(self):
+        return f'{self.user} subscription to {self.course}: {self.is_subscribed}'
+
+    class Meta:
+        verbose_name = 'подписка'
+        verbose_name_plural = 'подписки'
