1c662b71080c489bb3803cddcc7ca11e1ba54224
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-04 12:47:23 +0500

    Update Payment model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phonenumber_field import modelfields
@@ -25,15 +23,17 @@ class User(AbstractUser):
 
 
 class Payment(models.Model):
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='пользователь')
-    payment_date = models.DateTimeField(verbose_name='дата оплаты')
-    course = models.ForeignKey(to=Course, on_delete=models.CASCADE, verbose_name='оплаченный курс')
-    lesson = models.ForeignKey(to=Lesson, on_delete=models.CASCADE, verbose_name='оплаченный урок')
-    sum = models.PositiveIntegerField(verbose_name='сумма оплаты')
-    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD, verbose_name='способ оплаты')
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='пользователь', **NULLABLE)
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name='дата оплаты')
+    course = models.ForeignKey(to=Course, on_delete=models.CASCADE, verbose_name='оплаченный курс', **NULLABLE)
+    lesson = models.ForeignKey(to=Lesson, on_delete=models.CASCADE, verbose_name='оплаченный урок', **NULLABLE)
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD, default='transaction', verbose_name='способ оплаты')
+    session_id = models.CharField(max_length=255, verbose_name='id сессии', **NULLABLE)
+    payment_link = models.URLField(max_length=400, verbose_name='ссылка на оплату', **NULLABLE)
+    payment_status = models.CharField(max_length=30, verbose_name='статус платежа', **NULLABLE)
 
     def __str__(self):
-        return f'payment for {self.lesson} ({self.course}) / {self.user}'
+        return f'payment for {self.lesson if self.lesson else self.course} / {self.user}'
 
     class Meta:
         verbose_name = 'платеж'
