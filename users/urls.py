ecf938431b7538ca7ac82c08a37171b953e26050
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-04 12:52:53 +0500

    Add routers for  PaymentCreateAPIView + PaymentDetailView

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import (UserUpdateAPIView, PaymentListAPIView, UserListAPIView, UserCreateAPIView, UserRetrieveAPIView,
-                         UserDestroyAPIView)
+from users.views import (UserUpdateAPIView, PaymentListAPIView, PaymentCreateAPIView, UserListAPIView,
+                         UserCreateAPIView, UserRetrieveAPIView, UserDestroyAPIView, PaymentDetailView)
 
 app_name = UsersConfig.name
 
@@ -15,6 +15,9 @@ urlpatterns = [
     path('user/<int:pk>/', UserRetrieveAPIView.as_view(), name='user'),
     path('user/delete/<int:pk>/', UserDestroyAPIView.as_view(), name='delete_user'),
     path('payments/', PaymentListAPIView.as_view(), name='payments'),
+    path('payment/create/', PaymentCreateAPIView.as_view(), name='create_payment'),
     path('api/token/', TokenObtainPairView.as_view(permission_classes=[AllowAny]), name='create_token'),
-    path('api/token/refresh/', TokenRefreshView.as_view(permission_classes=[AllowAny]), name='refresh_token')
+    path('api/token/refresh/', TokenRefreshView.as_view(permission_classes=[AllowAny]), name='refresh_token'),
+
+    path('payment/success/<int:pk>/', PaymentDetailView.as_view(), name='success_url')
 ]
