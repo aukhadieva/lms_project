04e7fdb40acea67cedfaecc64efdd7cb557587b7
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-08 13:48:44 +0500

    Add CELERY_BROKER_URL + CELERY_RESULT_BACKEND

--- config/settings.py
+++ config/settings.py
@@ -173,3 +173,7 @@ SIMPLE_JWT = {
 
 # Stripe secret key
 STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
+
+# Celery
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
