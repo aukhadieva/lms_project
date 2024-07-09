362006966ff9366c003c5324ab79b6a1eb1ad27a
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-09 12:12:47 +0500

    Update CELERY_BEAT_SCHEDULE

--- config/settings.py
+++ config/settings.py
@@ -180,9 +180,9 @@ CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
 
 CELERY_BEAT_SCHEDULE = {
-    'task-name': {
+    'check last login': {
         'task': 'users.tasks.check_last_login',
-        'schedule': timedelta(minutes=10),
+        'schedule': timedelta(seconds=10),
     },
 }
 
