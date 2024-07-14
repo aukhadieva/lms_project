7176407afc822d54ac02b7ded97d53cd4908734a
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-14 11:56:07 +0500

    Update PaymentSerializer - add payment_status field

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,24 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from users.models import User, Payment
+from users.services import checkout_session
 
 
 class PaymentSerializer(serializers.ModelSerializer):
+    payment_status = SerializerMethodField()
+
     class Meta:
         model = Payment
         fields = '__all__'
+
+    def get_payment_status(self, obj):
+        """
+        Получает статус платежа.
+        """
+        session = obj.session_id
+        status = checkout_session(session)
+        return status
 
 
 class UserSerializer(serializers.ModelSerializer):
